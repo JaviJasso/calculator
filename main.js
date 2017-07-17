6b2894bc650b3112e828dbb8922a1ffa8f790cce
Javi Jasso <jjasso1@asu.edu>
2017-07-17 17:18:12 -0400

    background

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
-const numbers = document.querySelectorAll(".btn")
-const operaciones = document.querySelectorAll(".opt")
-const clear = document.querySelectorAll(".clear")
-const display = document.querySelector(".displayButton")
-const equla = document.querySelectorAll(".equal")
+let numbers = document.querySelectorAll(".btn")
+let operators = document.querySelectorAll(".opt")
+let clear = document.querySelector(".clearing")
+let display = document.querySelector(".displayButton")
+let equal = document.querySelector(".equal")
 
 let valor = ""
+let operator = ""
+let x = ""
+let y = ""
 
 for (let i = 0; i < numbers.length; i++) {
   let current = numbers[i]
   console.log(current)
   current.addEventListener("click", () => {
     let value = current.innerText
-    valor += value
+    valor += value // <====Cambia elvalor y pon como haras el X y Y para todas las opreaciones
     display.textContent = valor
   })
 }
+
+for (let i = 0; i < operators.length; i++) {
+  let current = operators[i]
+  console.log(current)
+  current.addEventListener("click", () => {
+    operator = current.textContent
+    console.log(operator)
+  })
+}
+
+equal.addEventListener("click", () => {
+  if (operator === "+") {
+    result = parseInt(x) + parseInt(y)
+  } else if (operator === "-") {
+    result = parseInt(x) - parseInt(y)
+  } else if (operator === "*") {
+    result = parseInt(x) * parseInt(y)
+  } else if (operator === "/") {
+    result = parseInt(x) / parseInt(y)
+  }
+  display.textContent = result
+})
+
+clear.addEventListener("click", () => {
+  valor = ""
+  display.textContent = valor
+})
